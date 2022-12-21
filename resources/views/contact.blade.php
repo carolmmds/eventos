93026ba170d2f6e07d2c7a32b3032c3208a3325d
Ana C do Nascimento Medeiros <ana.medeiros@PMCG.IMTI>
2022-12-21 10:43:17 -0400

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+{{-- <h1>Página de Contato!</h1>
+<a href="/">Voltar para home</a> --}}
+
+@extends('layouts.main')
+
+@section('title', 'Criar Evento')
+
+@section('content')
+<h1>sss</h1>
+@endsection
